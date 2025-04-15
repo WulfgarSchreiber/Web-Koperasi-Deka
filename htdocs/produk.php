f9dc1fcbfdf74bc6fb2e4a811708c6b2a725225b
Raihan Ardiansyah <112312624+WulfgarSchreiber@users.noreply.github.com>
2025-04-15 10:09:12 +0700

    Update produk.php

--- htdocs/produk.php
+++ htdocs/produk.php
@@ -209,7 +209,7 @@ $result = $conn->query($sql);
     	</div>
     </div>
 	<!-- section footer end -->
-	<div class="copyright">2025 Digawe Cah Pati. <a href="https://html.design">Okeh Sep Penting Dadi.</a></div>
+	<div class="copyright">PT. Dua Kelinci Pati&nbsp<a href="https://html.design">| Koperasi  Dua Kelinci.</a></div>
 
       <!-- Javascript files-->
       <script src="js/jquery.min.js"></script>
